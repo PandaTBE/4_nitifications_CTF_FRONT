6bfc1e538bab98a29c3dd4f53d455df9560283d8
Nikita Surov <surov.n2012@gmail.com>
2021-09-06 15:29:11 +0300

    поменял хост

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,6 @@
 const host = 'http://notifications.mctf-test.serega6531.ru';
 
 export const urls = {
-    GET_USER_ID: `${host}/backend/getUserId`,
-    GET_USER_NOTIFICATIONS: `${host}/backend/getNotifications?userId=`,
+    GET_USER_ID: `/backend/getUserId`,
+    GET_USER_NOTIFICATIONS: `/backend/getNotifications?userId=`,
 };
