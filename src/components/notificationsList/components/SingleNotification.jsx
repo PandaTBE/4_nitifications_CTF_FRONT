f1eaeee6705da5e48382f0141b4c1f81fd51fd75
Nikita Surov <surov.n2012@gmail.com>
2021-09-09 18:00:04 +0300

    изменил время

--- src/components/notificationsList/components/SingleNotification.jsx
+++ src/components/notificationsList/components/SingleNotification.jsx
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 import { NotificationsHeader, TextWrapper, StyledPaper } from './SingleNotification.styles';
 
 const SingleNotification = ({ date, text }) => {
-    const dt = DateTime.fromISO(date).toLocal().toFormat('HH:MM:ss');
+    const dt = DateTime.fromISO(date, { zone: 'utc' }).setZone('local').toFormat('HH:mm:ss');
     return (
         <StyledPaper elevation={3}>
             <NotificationsHeader>
